6cb9d63e7f181658ba4db5aa880a8d5c327b71c5
ErzaAdemaj <erzaademaj10@gmail.com>
2026-04-24 18:13:58 +0200

    add landingpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import AdminDashboard from "./layout/AdminDashboard.jsx";
 import About from "./pages/About.jsx";
 import Contact from "./pages/Contact.jsx";
+import LandingPage from "./landing/LandingPage.jsx";
 import SignIn from "./signin.jsx";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
+      <Route path="/landing" element={<LandingPage />} />
       <Route path="/admin" element={<AdminDashboard />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
